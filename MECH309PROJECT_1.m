4c9c180de3891c61ff720b68b1c5790305a654ba
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-11-23 15:11:41 -0500

    try
    This reverts commit f08a00d1bd950d01403ecf8ebeeb84f86d661bcc.

--- MECH309PROJECT_1.m
+++ MECH309PROJECT_1.m
@@ -1,17 +1,17 @@
 %%%                 MECH 309 - Numerical Methods in Mech Eng                 %%%
-
+ 
 % Presented to Prof Siva Nadarajah Winter 2019 - November 22th
-
+ 
 %Yiming Yao 260769906
 %Zechen Ren 260765431
 %
-
+ 
 clear all
 close all
 clc
  
 %Known Variables
-
+ 
 gamma = 1.4; %specific heat ratio for air
 R = 287.058; %J*kg^?1*K^?1 gas constant
 Tinf = 293; %K freestream static temperature
@@ -21,7 +21,7 @@ x = 50; %x-direction domain
 y = 50; %y-direction domain
 Minf = 0.2; %Mach number of freestream
 Uinf = Minf * c; %Flow speed of freestream 
-
+ 
 %Initializing Variables 
 Nx = 60; % x direction grid
 Ny = 60; % y direction grid
@@ -37,19 +37,19 @@ i = -1; % x direction tracing
  
 u_ = -1; % local phi derivative to x initialization
 v_ = -1; % local phi derivative to y initialization
-
+ 
 m = -1; % Mech number locally
 A = zeros (Nx,Ny); % initialize A matrix
-
+ 
 error = Inf;
 count = 0;
-
+ 
 % START
 %Boundary conditon
 while (error > 10^-2 && count < 1000)
     count = count + 1;
     phikm1 = phi; 
-
+ 
 %update phi at second bottom row, neummann at x != 20,21
 %update phi at second bottom row, neummann at x  = 20,21
 for  k = 2:Nx-1 
@@ -60,9 +60,10 @@ for  k = 2:Nx-1
         phi ( Nx, k) = phi ( Nx -1 , k) + dy *  Uinf * dydx;
     end
 end
-
+ 
 %A judgement
-
+<<<<<<< HEAD
+ 
 u_ = (phi(j,i-1) - phi(j,i+1))/(2*dx) ;
 v_ = (phi(j-1,i) - phi(j-1,i))/(2*dy) ;
 u = (u_ + Uinf);
@@ -80,7 +81,11 @@ else
     miu(j,i)= 1;
 end
     
-
+=======
+ 
+[A(j,i),miu(j,i)] = CalA_Miu(phi, i, j, Uinf, Minf, gamma, dx, dy);
+>>>>>>> 9ca54843f69ba4a66c3794bd9e6183ab3765324d
+ 
 %Point Gaussi-Seidal
 for i = 3 : Nx-1
     for j = 2 : Ny-1
@@ -93,7 +98,7 @@ for i = 3 : Nx-1
             ) / ( miu(j,i-1)*A(j,i-1) / (dx)^2 - 2*(1-miu(j,i))*A(j,i) / (dx)^2 - 2/(dy)^2 );
     end
 end
-
+ 
 error = norm (phikm1 - phi);
 end
 %Plots
