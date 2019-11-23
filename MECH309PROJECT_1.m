bb424cac61270e2dacbeca0b23b47b830194bb78
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-11-23 14:52:23 -0500

    A

--- MECH309PROJECT_1.m
+++ MECH309PROJECT_1.m
@@ -27,15 +27,21 @@ Uinf = Minf * c; %Flow speed of freestream
 %Initializing Variables 
 Nx = 60;
 Ny = 60;
-
+ 
 dx = x/Nx;
 dy = y/Ny;
-
-phi = zeros (Nx,Ny);
+ 
+phi = zeros (Nx,Ny); 
 miu = zeros (Nx,Ny);
-
-j = -1;
-i = -1;
+ 
+j = -1; % y direction tracing
+i = -1; % x direction tracing
+ 
+m = -1; % Mech number local
+A = zeros (Nx,Ny); % initialize A matrix
+ 
+u_ = -1; % local phi derivative to x
+v_ = -1; % local phi derivative to y
 
 error = Inf;
 count = 0;
@@ -59,7 +65,25 @@ end
 
 %A judgement
 
-u = diff (phi(j,i),i);
+%A judgement
+ 
+u_ = (phi(j,i-1) - phi(j,i+1))/(2*dx) ;
+v_ = (phi(j-1,i) - phi(j-1,i))/(2*dy) ;
+u = (u + Uinf);
+U = sqrt (u_^2 + v_^2);
+m = U / c;
+if m > 1 % supersonic locally
+    A (j,i) = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i) - phi (j,i-2) ) / ( 2* dx);
+else % subsonic flow
+    A (j,i) = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i+1) - phi (j,i-1) ) / ( 2* dx);
+end
+ 
+if A(j,i) > 0
+    miu(j,i) = 0;
+else
+    miu(j,i)= 1;
+end
+    
 
 %Point Gaussi-Seidal
 for i = 3 : Nx-1
