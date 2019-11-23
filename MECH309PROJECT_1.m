ca21b2b912a623429b47c70addc7ce2ee2960790
Yiming Yao <yiming.yao@mail.mcgill.ca>
2019-11-23 15:13:05 -0500

    calA

--- MECH309PROJECT_1.m
+++ MECH309PROJECT_1.m
@@ -62,29 +62,8 @@ for  k = 2:Nx-1
 end
  
 %A judgement
-<<<<<<< HEAD
- 
-u_ = (phi(j,i-1) - phi(j,i+1))/(2*dx) ;
-v_ = (phi(j-1,i) - phi(j-1,i))/(2*dy) ;
-u = (u_ + Uinf);
-U = sqrt (u^2 + v_^2);
-m = U / c;
-if m > 1 % supersonic locally
-    A (j,i) = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i) - phi (j,i-2) ) / ( 2* dx);
-else % subsonic flow
-    A (j,i) = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i+1) - phi (j,i-1) ) / ( 2* dx);
-end
- 
-if A(j,i) > 0
-    miu(j,i) = 0;
-else
-    miu(j,i)= 1;
-end
-    
-=======
- 
+
 [A(j,i),miu(j,i)] = CalA_Miu(phi, i, j, Uinf, Minf, gamma, dx, dy);
->>>>>>> 9ca54843f69ba4a66c3794bd9e6183ab3765324d
  
 %Point Gaussi-Seidal
 for i = 3 : Nx-1
