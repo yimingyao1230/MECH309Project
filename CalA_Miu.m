8ca3f8e5c1b40f082d3ace5010a89502b8cf61e9
Yiming Yao <yiming.yao@mail.mcgill.ca>
2019-11-23 19:47:17 -0500

    update calA

--- CalA_Miu.m
+++ CalA_Miu.m
@@ -1,15 +1,15 @@
-function [A, miu,cp] = CalA_Miu(phi, i, j, Uinf, Minf, gamma, dx, dy,c)
+function [A, miu, cp] = CalA_Miu(phi, counter, Nx, Uinf, Minf, gamma, dx, dy,c)
     %A judgement
  
-    u_ = (phi(j,i-1) - phi(j,i+1))/(2*dx) ;
-    v_ = (phi(j-1,i) - phi(j-1,i))/(2*dy) ;
+    u_ = (phi(counter+1,1) - phi(counter-1,1))/(2*dx) ;
+    v_ = (phi(counter+(Nx-3),1) - phi(counter-(Nx-3),1))/(2*dy) ;
     u = (u_ + Uinf);
     U = sqrt (u^2 + v_^2);
     m = U / c;
     if m > 1 % supersonic locally
-        A = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i) - phi (j,i-2) ) / ( 2* dx);
+        A = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (counter,1) - phi (counter-2,1) ) / ( 2* dx);
     else % subsonic flow
-        A = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (j,i+1) - phi (j,i-1) ) / ( 2* dx);
+        A = (1 - Minf)^ 2 - (gamma + 1) * Minf^2 / Uinf * (phi (counter+1,1) - phi (counter-1,1) ) / ( 2* dx);
     end
 
     if A > 0
