39e08efc506665fc4bf0a657adfb9e713f41caf7
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-12-01 13:50:38 -0500

    Difination

--- try1.m
+++ try1.m
@@ -4,7 +4,7 @@
  
 %Yiming Yao 260769906
 %Zechen Ren 260765431
- 
+%
 clear all
 close all
 clc
@@ -18,11 +18,11 @@ Pinf = 100; %kN/m^2 freestream static pressure
 C = 340; %m/s speed of sound, assumed constant ???
 x = 50; %x-direction domain
 y = 50; %y-direction domain
-Minf = 0.2; %Mach number of freestream
+Minf = 0.8; %Mach number of freestream
 Uinf = Minf*sqrt(gamma*R*Tinf); %Flow speed of freestream 
 toc = 0.08; % thickness ratio
-xle  = 20;
-xte  = 21;
+xle  = 20; % Airfoil Leading Edge
+xte  = 21; % Airfoil Trailing Edge
 %% Initializing Variables 
 dx = 0.1; % grid discrete distance
 dy = 0.1; % grid discrete distance
@@ -31,11 +31,12 @@ Nx = x/dx; % x direction grid
 Ny = y/dy; % y direction grid
  
 phi = zeros (Nx*Ny,1); % phi initialization
-phiNew = zeros (Nx*Ny,1); % phi initialization
+phiOld = zeros (Nx*Ny,1); % phiOld initialization
 miu = zeros (Nx*Ny,1); % miu initialization
 cp = zeros (Nx*Ny,1); % cp initialization
-plotphi = zeros(Nx,Ny);
+plotphi = zeros(Nx,Ny); % Converted Phi initialization
 
+% PDE Coefficient initialization
 a = zeros (Nx*Ny,1);
 b = zeros (Nx*Ny,1);
 c = zeros (Nx*Ny,1);
@@ -44,24 +45,24 @@ e = zeros (Nx*Ny,1);
 f = zeros (Nx*Ny,1);
 g = zeros (Nx*Ny,1);
  
-j = -1; % y direction tracing
-i = -1; % x direction tracing
- 
+% j = -1; % y direction tracing
+% i = -1; % x direction tracing
+%  
 u_ = -1; % local phi derivative to x initialization
 v_ = -1; % local phi derivative to y initialization
  
 m = -1; % Mech number locally
 A = zeros (Nx*Ny,1); % initialize A matrix
  
-error = Inf;
-tol = 1E-4;
-count = 0;
+error = Inf; % Begining with infinite error
+tol = 1E-4; % Tolerence
+count = 0; % Runtime counting
 
-xspan = linspace(0,x,Nx);
-dydx = toc * (-4 * xspan + 82);
-dydx(xspan<xle | xspan>xte ) = 0;
+xspan = linspace(0,x,Nx); % x discrete spacing
+dydx = toc * (-4 * xspan + 82); % Dy/Dx
+dydx(xspan<xle | xspan>xte ) = 0; % Zero Dy/Dx outside the airfoil range 
 
-errorlist = nan(1,1000);
+errorlist = nan(1,1000); % Storing error
 
 %% solve
 while error > tol
@@ -74,7 +75,8 @@ while error > tol
     for j = 2:(Ny-1)
         for i = 2:(Nx-1)
             loc = (j-1) * Ny + i;
-            A(loc,1) = (1 - Minf^2) - (gamma + 1) * Minf^2 / Uinf * (phi(loc+1,1) - phi(loc-1,1) ) / ( 2 * dx);
+            A(loc,1) = (1 - Minf^2) - (gamma + 1) * Minf^2 ...
+                / Uinf * (phi(loc+1,1) - phi(loc-1,1) ) / ( 2 * dx);
             if A(loc,1) > 0
                 miu(loc,1) = 0;
             else
@@ -82,17 +84,27 @@ while error > tol
             end
         end
     end
-    phiNew = phi;
+    % Storing phiOld for error computing
+    phiOld = phi;
     % factor
     for j = 2:(Ny-1)
         for i = 3:(Nx-1)
+            % Locating specific Phi position
             loc = (j-1) * Ny + i;
+            % Coefficient of (i,j-1) 
             c(loc,1) = 1 / dy^2;
+            % Coefficient of (i-2,j)
             g(loc,1) = miu(loc-1,1) * A(loc-1,1) / dx^2;
-            d(loc,1) = (1-miu(loc,1)) * A(loc,1) / (dx)^2 - 2 * miu(loc-1,1) * A(loc-1,1) / (dx)^2;
+            % Coefficient of (i-1,j)
+            d(loc,1) = (1-miu(loc,1)) * A(loc,1) / (dx)^2 ...
+                - 2 * miu(loc-1,1) * A(loc-1,1) / (dx)^2;
+            % Coefficient of (i+1,j)
             e(loc,1) = (1-miu(loc,1)) * A(loc,1) / (dx)^2;
+            % Coefficient of (i,j+1))
             b(loc,1) = 1 / dy^2;
+            % Coefficient of (i,j)
             a(loc,1) = miu(loc-1,1) * A(loc-1,1) / (dx)^2 - 2 * ((1-miu(loc,1))*A(loc,1))/(dx)^2 - 2/(dy)^2;
+            % Phi Computing
             phi(loc,1) = -(c(loc,1) * phi(loc-Nx,1) ...
                 + g(loc,1) * phi(loc-2,1) ...
                 + d(loc,1) * phi(loc-1,1) ...
@@ -102,19 +114,26 @@ while error > tol
         end
     end
 
-    error = max(max(abs(phiNew - phi)));
+    error = max(max(abs(phiOld - phi)));
     count = count + 1;
     errorlist (count) = error;
 end
 %% plot
+% Preparing Phi into [Nx,Ny] Matrix
 for j = 1 : Ny
     for i = 1 : Nx
         loc = (j-1) * Ny + i;
         plotphi(i,j) = phi(loc,1);
     end
 end
-pcolor(plotphi)
-colorbar
+
+
+%  for i = 2:Nx-1
+%         u_ = (plotphi(2,i+1) - plotphi(2,i-1))/(2*dx) ;
+%         cp (i)  = -2*u_/Uinf;
+%         
+%  end
+
 % semilog (errorlist);
 
 
