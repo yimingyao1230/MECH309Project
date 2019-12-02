bf2477408acc858575275223af37511e60ec5d26
zechen.ren@mail.mcgill.ca <zechen.ren@mail.mcgill.ca>
2019-12-01 20:45:34 -0500

    Final_with_x_not_solved

--- Mech_309_Project.m
+++ Mech_309_Project.m
@@ -52,7 +52,7 @@ m = -1; % Mech number locally
 A = zeros (Nx*Ny,1); % initialize A matrix
  
 error = Inf; % Begining with infinite error
-tol = 1E-4; % Tolerence
+tol = 1E-2; % Tolerence
 count = 0; % Runtime counting
 
 xspan = linspace(0,x,Nx); % x discrete spacing
@@ -61,12 +61,12 @@ xspan = linspace(0,x,Nx); % x discrete spacing
 
 errorlist = nan(1,1000); % Storing error
 
-%% Question 1
-% [phi,miu,A,errorlist,count] = ...
-% MurmanColeSolver(phi,miu,A,a,b,c,d,e,g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,dydx,dy,dx,count);
+% %% Question 1
+% % [phi,miu,A,errorlist,count] = ...
+% % MurmanColeSolver(phi,miu,A,a,b,c,d,e,g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,dydx,dy,dx,count);
+% 
 
-%% plot
-% Problem 2
+%% Problem 2
 for l = 1:6
     legends{l}=  ['Mach # = ' num2str(0.73+0.02*l)];
     hold on
@@ -111,7 +111,7 @@ g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,Pinf,dydx,dy,dx,count);
  % Error
 figure (21)
 semilogy(errorlist)
-xlabel('x')
+xlabel('Interations')
 ylabel('Error')
 title('Q2 Error')
 legend(legends)
@@ -139,18 +139,110 @@ title(str)
 end
 
 
-% %% Question3
-% Minf = 0.8;
-% for dx = 0.025:0.025:0.1 % grid discrete distance
-%     dy = dx;
-%     [phi,miu,A,errorlist,count] = ...
-%         MurmanColeSolver(phi,miu,A,a,b,c,d,e,g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,dydx,dy,dx,count);
-% end
-% 
-% %% Question4
-% dx = 0.05;
-% dy = 0.05;
-% for Minf = 0.75:0.02:0.85
-%     [phi,miu,A,errorlist,count] = ...
-%         MurmanColeSolver(phi,miu,A,a,b,c,d,e,g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,dydx,dy,dx,count);
-% end
+%% Question3
+Minf = 0.8;
+for dx = [0.025,0.05,0.1] % grid discrete distance
+    dy = dx;
+% REinitializing Variables 
+Nx = x/dx; % x direction grid
+Ny = y/dy; % y direction grid
+
+phi = zeros (Nx*Ny,1); % phi initialization
+phiOld = zeros (Nx*Ny,1); % phiOld initialization
+miu = zeros (Nx*Ny,1); % miu initialization
+
+% PDE Coefficient initialization
+a = zeros (Nx*Ny,1);
+b = zeros (Nx*Ny,1);
+c = zeros (Nx*Ny,1);
+d = zeros (Nx*Ny,1);
+e = zeros (Nx*Ny,1);
+f = zeros (Nx*Ny,1);
+g = zeros (Nx*Ny,1);
+
+m = -1; % Mech number locally
+A = zeros (Nx*Ny,1); % initialize A matrix
+
+error = Inf; % Begining with infinite error
+count = 0; % Runtime counting
+
+xspan = linspace(0,x,Nx); % x discrete spacing
+dydx = toc * (-4 * xspan + 82); % Dy/Dx
+dydx(xspan<xle | xspan>xte ) = 0; % Zero Dy/Dx outside the airfoil range 
+
+errorlist = nan(1,1000); % Storing error
+
+[plotphi,cp,p,errorlist,count] = ...
+MurmanColeSolver(phi,miu,A,a,b,c,d,e, ...
+g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,Pinf,dydx,dy,dx,count);
+
+% Cp Ploting
+%Cp Ploting
+figure (22)
+plot (cp);
+title('cp');
+xlim([19.5/dx,21.5/dx]);
+ylim([-0.5,1]);
+xlabel('x')
+ylabel('$C_p$','interpreter','latex')
+title('Q2 Coefficient of Pressure')
+%legend(legends)
+%hold on
+
+end
+
+%% Question4
+for Minf = 0.75:0.02:0.85
+    for l = 1:6
+    legends{l}=  ['Mach # = ' num2str(0.73+0.02*l)];
+    end
+end
+for Minf = 0.75:0.02:0.85
+% REinitializing Variables 
+dx = 0.025; % grid discrete distance
+dy = dx; % grid discrete distance
+
+Nx = x/dx; % x direction grid
+Ny = y/dy; % y direction grid
+
+phi = zeros (Nx*Ny,1); % phi initialization
+phiOld = zeros (Nx*Ny,1); % phiOld initialization
+miu = zeros (Nx*Ny,1); % miu initialization
+
+% PDE Coefficient initialization
+a = zeros (Nx*Ny,1);
+b = zeros (Nx*Ny,1);
+c = zeros (Nx*Ny,1);
+d = zeros (Nx*Ny,1);
+e = zeros (Nx*Ny,1);
+f = zeros (Nx*Ny,1);
+g = zeros (Nx*Ny,1);
+
+m = -1; % Mech number locally
+A = zeros (Nx*Ny,1); % initialize A matrix
+
+error = Inf; % Begining with infinite error
+count = 0; % Runtime counting
+
+xspan = linspace(0,x,Nx); % x discrete spacing
+dydx = toc * (-4 * xspan + 82); % Dy/Dx
+dydx(xspan<xle | xspan>xte ) = 0; % Zero Dy/Dx outside the airfoil range 
+
+errorlist = nan(1,1000); % Storing error
+
+[plotphi,cp,p,errorlist,count] = ...
+MurmanColeSolver(phi,miu,A,a,b,c,d,e, ...
+g,error,errorlist,tol,Nx,Ny,gamma,Uinf,Minf,Pinf,dydx,dy,dx,count);
+
+% Cp Ploting
+figure (42)
+plot (cp);
+title('cp');
+xlim([19.5/dx,21.5/dx]);
+ylim([-0.5,1]);
+xlabel('x')
+ylabel('$C_p$','interpreter','latex')
+title('Q4 Coefficient of Pressure')
+legend(legends)
+hold on
+end
